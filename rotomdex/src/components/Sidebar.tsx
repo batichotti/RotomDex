b221cfd08e22ea01219454cceaf8f7292c95e66c
ArthurvMachado <avm.arthurv@gmail.com>
2026-04-24 12:46:20 -0300

    Implement Sidebar

--- rotomdex/src/components/Sidebar.tsx
+++ rotomdex/src/components/Sidebar.tsx
@@ -3,13 +3,21 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import styles from './Sidebar.module.css'
+import Image from 'next/image'
 
 export default function Sidebar() {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
         <>
-            <button onClick={() => setIsOpen(true)}>☰</button>
+            <button className={styles.menuBtn} onClick={() => setIsOpen(true)}>
+                <Image
+                    src="/logos/.png/RotomDex_mini_colored.png"
+                    alt="Menu"
+                    width={90}
+                    height={53}
+                />
+            </button>
 
             {isOpen && (
                 <div
